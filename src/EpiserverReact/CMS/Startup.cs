4bec3124ddb72c153fc559054ccfaa3330ec846e
Drew Null <drew@drewnull.com>
2019-04-06 13:41:03 -0500

    Configure Episerver Service API

---
+++ src/EpiserverReact/CMS/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CMS.Startup))]
+
+namespace CMS
+{
+    using EPiServer.ServiceApi.Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable bearer token authentication using Membership for Service Api
+            app.UseServiceApiMembershipTokenAuthorization(new ServiceApiTokenAuthorizationOptions
+            {
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60)
+            });
+        }
+    }
+}
