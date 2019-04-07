c1dd8579ef756c526766f960cc1455385fa9130a
Drew Null <drew@drewnull.com>
2019-04-07 01:13:38 -0500

    Add content area expansion

---
+++ src/EpiserverReact/CMS/Infrastructure/CustomContentAreaPropertyConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace CMS.Infrastructure
+{
+    using System.Collections.Generic;
+    using EPiServer.ContentApi.Core.Serialization;
+    using EPiServer.ContentApi.Core.Serialization.Internal;
+    using EPiServer.ContentApi.Core.Serialization.Models;
+    using EPiServer.ServiceLocation;
+    using EPiServer.SpecializedProperties;
+
+    /// <summary>
+    /// Custom converter for <see cref="PropertyContentArea" /> so Headless will use our custom custom view model when convert this property/>
+    /// </summary>
+    [ServiceConfiguration(typeof(IPropertyModelConverter), Lifecycle = ServiceInstanceScope.Singleton)]
+    public class CustomContentAreaPropertyConverter : DefaultPropertyModelConverter
+    {
+        /// <summary>
+        /// The Default converter of Headless has SortOrder = 0, so our custom converter should have this property with value greater than 0
+        /// so Headless will prioritize the custom converter over the default converter.
+        /// </summary>
+        public override int SortOrder { get; } = 100;
+
+        protected override IEnumerable<TypeModel> InitializeModelTypes()
+        {
+            var typeList = new List<TypeModel>();
+            typeList.Add(new TypeModel() { PropertyType = typeof(PropertyContentArea), ModelType = typeof(CustomContentAreaPropertyModel), ModelTypeString = typeof(CustomContentAreaPropertyModel).FullName });
+            return typeList;
+        }
+    }
+}
---
+++ src/EpiserverReact/CMS/Infrastructure/CustomContentAreaPropertyModel.cs
@@ -0,0 +1,42 @@
+﻿namespace CMS.Infrastructure
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using EPiServer.ContentApi.Core.Serialization.Models;
+    using EPiServer.Core;
+    using EPiServer.Security;
+    using EPiServer.SpecializedProperties;
+
+    /// <summary>
+    /// Custom view model to mapped with <see cref="PropertyContentArea"/>
+    /// </summary>
+    public class CustomContentAreaPropertyModel : ContentAreaPropertyModel
+    {
+        public CustomContentAreaPropertyModel(
+            PropertyContentArea propertyContentArea,
+            bool excludePersonalizedContent) : base(propertyContentArea, excludePersonalizedContent)
+        {      
+		
+        }
+
+        /// <summary>
+        /// Herre we override Expand behaviour of this model and expands all level 
+        /// </summary>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        protected override IEnumerable<ContentApiModel> ExtractExpandedValue(CultureInfo language)
+        {
+            var expandedValue = new List<ContentApiModel>();
+
+            var contentReferences = this.Value.Where(x => x.ContentLink != null).Select(x => new ContentReference(x.ContentLink.Id.Value));
+            var content = this._contentLoaderService.GetItems(contentReferences, language).ToList();
+
+            var principal = this.ExcludePersonalizedContent ? this._principalAccessor.GetAnonymousPrincipal() : this._principalAccessor.GetCurrentPrincipal();
+            var filteredContent = content.Where(x => this._accessEvaluator.HasAccess(x, principal, AccessLevel.Read)).ToList();
+
+            filteredContent.ForEach(x => expandedValue.Add(this._contentModelMapper.TransformContent(x, this.ExcludePersonalizedContent, "*")));
+            return expandedValue;
+        }
+    }
+}
