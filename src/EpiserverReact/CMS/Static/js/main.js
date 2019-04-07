b6b60dc4707602f6ed2f3200fdd4b609d830f8ec
Drew Null <drew@drewnull.com>
2019-04-07 01:14:06 -0500

    Add content area React rendering

--- src/EpiserverReact/CMS/Static/js/main.js
+++ src/EpiserverReact/CMS/Static/js/main.js
@@ -2,14 +2,86 @@ const main = document.querySelector('#main');
 const episerverUrl = main.dataset.episerverUrl;
 const episerverType = main.dataset.episerverType;
 
-class HomePage extends React.Component {
+function getContentType(data) {
+    return data.contentType[data.contentType.length - 1];
+}
+
+function getContentAreaItems(contentArea) {
+    let components = [];
+    const items = contentArea.expandedValue;
+    if (items && items.length > 0) {
+        for (var index = 0; index < items.length; index++) {
+            let item = items[index];
+            let contentType = getContentType(item);
+            let ContentType = ContentTypes[contentType];
+            components.push(<ContentType data={item} key={index} />);
+        }
+    }
+    return components;
+}
+
+class BannerBlock extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
             <div>
+                <h2>Banner Block: {this.props.data.name}</h2>
+            </div>
+        );
+    }
+}
+
+class GridBlock extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2>Grid Block: {this.props.data.name}</h2>
+            </div>
+        );
+    }
+}
+
+class RichTextBlock extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2>Rich Text Block: {this.props.data.name}</h2>
+            </div>
+        );
+    }
+}
+
+const ContentTypes = {
+    'BannerBlock': BannerBlock,
+    'GridBlock': GridBlock,
+    'RichTextBlock': RichTextBlock
+};
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div>
                 <h1>Home Page</h1>
+                <div>
+                    Main Content:
+                    {getContentAreaItems(this.props.data.mainContent)}
+                </div>
+                <div>
+                    Side Content:
+                    {getContentAreaItems(this.props.data.sideContent)}
+                </div>
             </div>
         );
     }
@@ -19,22 +91,21 @@ const PageTypes = {
     'HomePage': HomePage
 };
 
-const BlockTypes = {
-    
-};
-
 const pageRequest = {
     method: 'get',
     url: episerverUrl,
     headers: {
         'accept': 'application/json'
+    },
+    params: {
+        'expand': '*'
     }
 };
 
 const renderPage = (response) => {
-    let pageType = response.data.contentType[response.data.contentType.length - 1];
+    let pageType = getContentType(response.data);
     let PageType = PageTypes[pageType];
-    ReactDOM.render(<PageType data={response.data} />, main);
+    ReactDOM.render(<PageType data={response.data} key={response.data.contentLink.id} />, main);
 };
 
 const handleError = (error) => {
